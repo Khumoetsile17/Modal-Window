46adbca787ab17eabe35d02310a66308a0ef547d
Khumoetsile17 <maropekhumoetsile@gmail.com>
2024-07-08 20:45:13 +0200

    Update all the files

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const buttonCloseModal_X = document.querySelector(".close-modal");
+const buttonsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < buttonsOpenModal.length; i++)
+  buttonsOpenModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+const closeModal = function () {
+  console.log("Button X or Outside modal clicked");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+buttonCloseModal_X.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
